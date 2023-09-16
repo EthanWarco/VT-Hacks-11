9d33ce4561a8914a72c96f4db28b5d3685b3282a
BuildTools <unconfigured@null.spigotmc.org>
2023-09-15 23:27:07 -0400

    Starting

---
+++ dorm-wars/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Leaderboard from './Leaderboard.js';
+
+function App() {
+  return (
+    <div className = "header">
+      <Leaderboard />
+    </div>
+  );
+}
+
+export default App;
---
+++ dorm-wars/src/Leaderboard.js
@@ -0,0 +1,10 @@
+import './Leaderboard.css';
+
+function Leaderboard() {
+    return (
+        <div class = "header">
+            <h1>Leaderboard goes here</h1>
+        </div>
+    );
+}
+export default Leaderboard;
