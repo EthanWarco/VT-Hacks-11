35396a3d7b95e53fa09b48a96adc8377b8eaf9c0
Andrew Schineller <therealcatberg@outlook.com>
2023-09-16 02:30:03 -0400

    implement mongodb api

--- dorm-wars/src/Leaderboard.js
+++ dorm-wars/src/Leaderboard.js
@@ -1,9 +1,15 @@
 import './Leaderboard.css';
 
+function getTopDorms() {
+    let dormsData = fetch("hjo")
+    return dormsData;
+}
+
 function Leaderboard() {
     return (
         <div class = "header">
             <h1>Leaderboard goes here</h1>
+            <p>{getTopDorms()}</p>
         </div>
     );
 }
