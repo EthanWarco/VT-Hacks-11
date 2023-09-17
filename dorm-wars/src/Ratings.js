ee4d6963bcc4ad6944927e826e8d017b7e59435b
Sriram P <sriram.mittu@gmail.com>
2023-09-17 00:49:34 -0400

    new font

--- dorm-wars/src/Ratings.js
+++ dorm-wars/src/Ratings.js
@@ -30,41 +30,41 @@ export default function Ratings() {
         <div>
             <h1>Ratings</h1>
             <ul>
-                <li><button class="">Ambler Johnston (East) </button></li>
-                <li><button class="">Ambler Johnston (West)                               </button></li>
-                <li><button class="">Campbell (Main)                                      </button></li>
-                <li><button class="">Campbell (East)                                      </button></li>
-                <li><button class="">Cochrane                                             </button></li>
-                <li><button class="">The Creativity and Innovation District Residence Hall</button></li>
-                <li><button class="">Eggleston (East)                                     </button></li>
-                <li><button class="">Eggleston (Main)                                     </button></li>
-                <li><button class="">Eggleston (West)                                     </button></li>
-                <li><button class="">Graduate Life Center at Donaldson Brown              </button></li>
-                <li><button class="">Harper                                               </button></li>
-                <li><button class="">Hillcrest                                            </button></li>
-                <li><button class="">Hoge Hall                                            </button></li>
-                <li><button class="">Johnson                                              </button></li>
-                <li><button class="">Miles                                                </button></li>
-                <li><button class="">New Hall West                                        </button></li>
-                <li><button class="">New Residence Hall East                              </button></li>
-                <li><button class="">Newman                                               </button></li>
-                <li><button class="">O'Shaughnessy Hall                                   </button></li>
-                <li><button class="">Payne                                                </button></li>
-                <li><button class="">Pearson Hall East                                    </button></li>
-                <li><button class="">Pearson Hall West                                    </button></li>
-                <li><button class="">Peddrew-Yates                                        </button></li>
-                <li><button class="">Pritchard Hall                                       </button></li>
-                <li><button class="">Slusher Hall                                         </button></li>
-                <li><button class="">Upper Quad Hall North                                </button></li>
-                <li><button class="">Vawter                                               </button></li>
-                <li><button class="">Whitehurst Hall                                      </button></li>
+                <li>Ambler Johnston (East)                               </li>
+                <li>Ambler Johnston (West)                               </li>
+                <li>Campbell (Main)                                      </li>
+                <li>Campbell (East)                                      </li>
+                <li>Cochrane                                             </li>
+                <li>The Creativity and Innovation District Residence Hall</li>
+                <li>Eggleston (East)                                     </li>
+                <li>Eggleston (Main)                                     </li>
+                <li>Eggleston (West)                                     </li>
+                <li>Graduate Life Center at Donaldson Brown              </li>
+                <li>Harper                                               </li>
+                <li>Hillcrest                                            </li>
+                <li>Hoge Hall                                            </li>
+                <li>Johnson                                              </li>
+                <li>Miles                                                </li>
+                <li>New Hall West                                        </li>
+                <li>New Residence Hall East                              </li>
+                <li>Newman                                               </li>
+                <li>O'Shaughnessy Hall                                   </li>
+                <li>Payne                                                </li>
+                <li>Pearson Hall East                                    </li>
+                <li>Pearson Hall West                                    </li>
+                <li>Peddrew-Yates                                        </li>
+                <li>Pritchard                                            </li>
+                <li>Slusher Hall                                         </li>
+                <li>Upper Quad Hall North                                </li>
+                <li>Vawter                                               </li>
+                <li>Whitehurst Hall                                      </li>
             </ul>
             <div class="area">
                 <h2>Submit Review</h2>
                 <form onSubmit={submitReview}>
-                    <input name="dormName" onChange={handleInputChange} placeholder="Name of the Dorm" />
-                    <input name="userName" onChange={handleInputChange} placeholder="First and Last Name" />
-                    <input name="body" onChange={handleInputChange} style={{ height: 200 }} placeholder="Enter upto 500 words" />
+                    <input name="dormName" onChange={handleInputChange} style={{ width: 600 }} placeholder="Name of the Dorm" />
+                    <input name="userName" onChange={handleInputChange} style={{ width: 600 }} placeholder="First and Last Name" />
+                    <input name="body" onChange={handleInputChange} style={{ height: 200, width: 600 }} placeholder="Enter upto 500 words" />
                     <input type="submit"></input>
                 </form>
             </div>
---
+++ dorm-wars/src/SignUp.js
@@ -0,0 +1,12 @@
+import "./SignUp.css";
+import { Auth } from "./components/auth";
+
+function SignUp() {
+    return (
+        <div className="App">
+            <Auth />
+        </div>
+    );
+}
+
+export default SignUp;
